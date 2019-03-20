ff523c25f42b4121f3e7655da595c9082533190c
sarahahmed911 <16435697+sarahahmed911@users.noreply.github.com>
2019-03-20 19:22:04 +0500

    Update whitening_transform.m

--- whitening_transform.m
+++ whitening_transform.m
@@ -1,5 +1,5 @@
-function [transformed_trainfeatures,transformed_testfeatures]= whitening_transform(train_data,test_data)
-cov_x=cov(train_data);
+function [transformed_trainfeatures,transformed_testfeatures]= whitening_transform(diversified_trainfea,diversified_testfea)
+cov_x=cov(diversified_trainfea);
 [eigvect,eigval] =svd(cov_x); 
 % CALCULATE D^(-1/2)
 eigval1 = diag(eigval);
@@ -8,9 +8,9 @@ eigval3= diag(d);
  
 % CALCULATE WHITENING TRANSFORM
 W = eigvect*eigval3*eigvect';
-for i=1:size(train_data,2)
- transformed_trainfeatures(:,i)=train_data(:,i)-mean(train_data(:,i));
+for i=1:size(diversified_trainfea,2)
+ transformed_trainfeatures(:,i)=diversified_trainfea(:,i)-mean(diversified_trainfea(:,i));
 end
-for i=1:size(x_test,2)
- transformed_testfeatures(:,i)=test_data(:,i)-mean(test_data(:,i));
+for i=1:size(diversified_testfea,2)
+ transformed_testfeatures(:,i)=diversified_testfea(:,i)-mean(diversified_testfea(:,i));
 end
