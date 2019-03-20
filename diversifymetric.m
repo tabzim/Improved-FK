6633683a6865c106a9df29d3a0746d0b8b724bc3
sarahahmed911 <16435697+sarahahmed911@users.noreply.github.com>
2019-03-20 17:44:55 +0500

    Update diversifymetric.m

--- diversifymetric.m
+++ diversifymetric.m
@@ -1,15 +1,15 @@
  %% Main file for Diversity Calculation of Fisher scores
  
-load('/data/usps_dbm_lastlayer.mat');
-%x=vertcat(train_x,test_x);
-
+[diversity_metric]=diversify_metric(norm_trainfea,norm_testfeat)
+ 
+number_hid_uits=80;
 % Calculating Theta
 dot_product=dot(train_data,train_data);
 norm_vect=norm(train_data)*norm(train_data);
 theta=acos(dot_product/norm_vect);
 
 % Calculating Mean and Variance
-total_hid_units=(80)^2;
+total_hid_units=(number_hiddenuits)^2;
 mean_angle=theta/total_hid_units;
 total_mean_angle=sum(mean_angle);
 variance=(theta-total_mean_angle).^2;
@@ -18,7 +18,3 @@ total_variance_angle=sum(variance_angle);
 
 % Calculating Diversity
 diversity_metric=total_mean_angle-total_variance_angle;
-
-display(diversity_metric)
-display(total_variance_angle)
-display(total_mean_angle)
